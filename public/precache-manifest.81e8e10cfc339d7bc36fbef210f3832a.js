ea8f70cc8b516f671f512ef0554601296d94c6b2
jan <apollg3@gmail.com>
2019-09-26 15:18:44 +0800

    Finished GraphQL with React & Apollo

---
+++ public/precache-manifest.81e8e10cfc339d7bc36fbef210f3832a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d58f2f1e6454838648f41a259c925d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc284ab1a3212d6142d0",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "0f2dd8b7f19d857e7ba7",
+    "url": "/static/js/2.6f26e7c3.chunk.js"
+  },
+  {
+    "revision": "cc284ab1a3212d6142d0",
+    "url": "/static/js/main.b1c0520b.chunk.js"
+  },
+  {
+    "revision": "09e27f03fdb94530568a",
+    "url": "/static/js/runtime-main.9d4f7790.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
